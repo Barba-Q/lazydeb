bfaa6dfa75b79510c5bb9217bf95146f46d96046
Barba-Q <46282569+Barba-Q@users.noreply.github.com>
2025-09-03 11:23:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,1 @@
-#!/bin/sh
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-else
-  cp $PWD/update-system.service 	/etc/systemd/system/update-system.service
-  cp $PWD/update-system.time 	/etc/systemd/system/update-system.time
-  cp $PWD/lazydeb.sh		$HOME/.lazydeb.sh
-  systemctl --system enable --now update-system.time
-  echo "all done, you may reboot your system now"
-fi
-
 
